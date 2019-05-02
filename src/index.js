15b831113137e55d8071a7a5453207dcdbd91104
BrunoAndreDias <baad@sapo.pt>
2019-05-02 13:55:15 +0100

    Image upload. Need rework of this to use images from a server or to have a specific dir for images

--- src/index.js
+++ src/index.js
@@ -4,19 +4,45 @@ import Discord from 'discord.js';
 const client = new Discord.Client();
 
 client.on('message', msg => {
-  console.log(msg);
-  if (msg.content.includes('!messias')) {
+  //Prevent from responding to himself
+  if (msg.author == client.user) {
+    return
+  }
+  if (msg.content.includes('messias')) {
     msg.channel.send(
       `Olá ${
-        msg.author
+      msg.author
       }! Eu sou o messias! Nada temas, pois comigo ultrapassarás todos os teus medos!`
     );
+  }
+
+  if (msg.content.includes('fapo')) {
+    //https://i.ytimg.com/vi/Owj-FUyHlcA/maxresdefault.jpg
+    const localFileAttachment = new Discord.Attachment('C:\\Users\\vitor silva\\Desktop\\elfapo.jpg');
+
+    msg.channel.send(
+      `Olá ${
+      msg.author
+      }! Sabias que o El Fapo aka 4funSW ou Fapo é ganda noob? Check this out => `
+    );
+    msg.channel.send(localFileAttachment);
+    msg.channel.send(
+      `Agora imagina com quem faz dupla!!`
+    );
+
+  }
+
+  if (msg.content.includes('dupla')) {
+    const mica = new Discord.Attachment('C:\\Users\\vitor silva\\Desktop\\xpeaker.jpg');
+
+    msg.channel.send(`Tira as tuas conclusões!`);
+    msg.channel.send(mica);
   }
 });
 
 client.on('ready', () => {
   client.channels
-    .find(x => x.name === 'geral')
+    .find(x => x.name === 'general')
     .send('O Messias está aqui! Um cristão não teme!');
 });
 
